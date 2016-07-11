ae68a0da9835cda00cf30575dd29cdba4ab7b351
laurencechan <laurencechan.qq.com>
2016-07-11 16:59:59 +0800

    品深度板块初版（待完善）

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from longwang.index_views import index_page
 from longwang.kbg_views import kbg_page
+from longwang.psd_views import psd_page
 import sys
 reload(sys)
 sys.setdefaultencoding("utf8")
@@ -8,7 +9,8 @@ sys.setdefaultencoding("utf8")
 app = Flask(__name__)
 app.register_blueprint(index_page)
 app.register_blueprint(kbg_page)
+app.register_blueprint(psd_page)
 
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(debug=True)

84d1df0961f1c74caf4710f1ff71186519eedfe5
laurencechan <laurencechan.qq.com>
2016-07-19 15:53:51 +0800

    品深度 图片显示调整

--- longwang/psd_views.py
+++ longwang/psd_views.py
@@ -91,7 +91,7 @@ def kbg_list(channel, page=1):
         style = 'style="display: block"'
         if i["Guideimage"] == "":
             style = 'style="display: none"'
-        value += "<li><p %s><a href='/detail/%s' target='_blank'><img src='%s' width='261' height='171'/></a></p><h2><a href='/detail/%s' target='_blank'>%s</a></h2> \
+        value += "<li><p %s><a href='/detail/%s' target='_blank'><img src='%s?w=261&h=171' width='261' height='171'/></a></p><h2><a href='/detail/%s' target='_blank'>%s</a></h2> \
         <h5>%s</h5> <h6>&nbsp;&nbsp;&nbsp;%s</h6></li>" % (
         style, i["_id"], image_server + i["Guideimage"], i["_id"], i["Title"], i["Summary"],
         datetime_op((i["Published"])))
@@ -159,7 +159,7 @@ def news_list_page(channel, page=1):
         style = 'style="display: block"'
         if i["Guideimage"] == "":
             style = 'style="display: none"'
-        value += "<li><p %s><img src='%s' width='261' height='171'/></p><h2><a href='/detail/%s' target='_blank'>%s</a></h2> <h5>%s</h5> <h6>&nbsp;&nbsp;&nbsp;%s</h6></li>" % \
+        value += "<li><p %s><img src='%s?w=261&h=171' width='261' height='171'/></p><h2><a href='/detail/%s' target='_blank'>%s</a></h2> <h5>%s</h5> <h6>&nbsp;&nbsp;&nbsp;%s</h6></li>" % \
                  (style, image_server + i["Guideimage"], i["_id"], i["Title"], i["Summary"],
                   datetime_op((i["Published"])))
     return json.dumps(value)

10e8798ac3d492d7f5c220e523bdfecda13e3ecf
laurencechan <laurencechan.qq.com>
2016-07-21 14:20:48 +0800

    品深度 侧边栏：今日热评重复问题解决

--- longwang/mongodb_news.py
+++ longwang/mongodb_news.py
@@ -162,7 +162,7 @@ def get_image_news(channel, limit, list_db=[]):
         id_list = []
         for news_list in list_db:
             id_list.append(news_list["_id"])
-        condition.update({"_id": {"$nin": id_list}})
+        condition.update({"NewsID": {"$nin": id_list}})
     lht = db.IndexNews.find(condition).sort("no", pymongo.DESCENDING).limit(limit)
     _lht = []
     for i in lht:

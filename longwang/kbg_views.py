3b494d9af2143a017cab9fa4ff95ea78603e4b11
laurencechan <laurencechan.qq.com>
2016-08-05 16:15:36 +0800

    龙网 url缩短计划

--- longwang/kbg_views.py
+++ longwang/kbg_views.py
@@ -69,26 +69,28 @@ def kbg_index():
 
 
 # 二级频道列表
-@kbg_page.route('/kbg/<channel>/<page>/')
-def kbg_list(channel, page=1):
-        condition = {"Channel": {"$in": [ObjectId(channel)]}, "Status": 4}
-        news_list = db.News.find(condition).sort('Published', pymongo.DESCENDING).skip(
-            pre_page * (int(page) - 1)).limit(
-            pre_page)
-        value = ""
-        for i in news_list:
-            style = 'style="display: block"'
-            if i["Guideimage"] == "":
-                style = 'style="display: none"'
-            value += "<li><p %s><a href='/detail/%s' target='_blank'><img src='%s?w=261&h=171' width='261' height='171'/></a></p><h2><a href='/detail/%s' target='_blank'>%s</a></h2> <h5>%s</h5> <h6>&nbsp;&nbsp;&nbsp;%s</h6></li>" % \
-                     (style, i["_id"], image_server + i["Guideimage"], i["_id"], i["Title"], i["Summary"],
-                      datetime_op((i["Published"])))
-        return json.dumps(value)
+@kbg_page.route('/kbg/<id>/<page>/')
+def kbg_list(id, page=1):
+    channel = db.Channel.find_one({"numid": int(id)})["_id"]
+    condition = {"Channel": {"$in": [ObjectId(channel)]}, "Status": 4}
+    news_list = db.News.find(condition).sort('Published', pymongo.DESCENDING).skip(
+        pre_page * (int(page) - 1)).limit(
+        pre_page)
+    value = ""
+    for i in news_list:
+        style = 'style="display: block"'
+        if i["Guideimage"] == "":
+            style = 'style="display: none"'
+        value += "<li><p %s><a href='/detail/%s' target='_blank'><img src='%s?w=261&h=171' width='261' height='171'/></a></p><h2><a href='/detail/%s' target='_blank'>%s</a></h2> <h5>%s</h5> <h6>&nbsp;&nbsp;&nbsp;%s</h6></li>" % \
+                 (style, i["_id"], image_server + i["Guideimage"], i["numid"], i["Title"], i["Summary"],
+                  datetime_op((i["Published"])))
+    return json.dumps(value)
 
 
 # 二级频道列表
-@kbg_page.route('/kbg/list/<channel>/')
-def kbg_list_index(channel):
+@kbg_page.route('/kbg/list/<id>/')
+def kbg_list_index(id):
+    channel = db.Channel.find_one({"numid": int(id)})["_id"]
     # 轮换图
     lht = get_head_image(ObjectId(channel), 4)
     news_list = search_news_db([ObjectId(channel)], pre_page)

cba91bb1b728c5165652dfff181e67405bb0a9f5
eiffel <wanglina_@126.com>
2016-08-23 16:48:24 +0800

    site-->modify little problem

--- static/js/sousuo.js
+++ static/js/sousuo.js
@@ -2,23 +2,23 @@
  * Created by wanglina on 2016/7/7.
  */
 function js_method(key) {
-    // var a = ""
-    //if (key == null || key==undefined ) {
-    //    a = encodeURI($("#keyword").val());
-    //    if (a == "") {
-    //        return false;
-    //    }
-    //}
-    //else {
-    //    a = key;
-    //}
-    //if (a == "") {
-    //    return false;
-    //} else {
-    //    var url = "http://www.hljss.com" + encodeURI("/s/" + key + "/");
-        var url = encodeURI("/ss/" + key + "/");
+     var a = ""
+    if (key == null || key==undefined ) {
+        a = encodeURI($("#keyword").val());
+        if (a == "") {
+            return false;
+        }
+    }
+    else {
+        a = key;
+    }
+    if (a == "") {
+        return false;
+    } else {
+        var url = "http://www.hljss.com" + encodeURI("/s/" + a + "/");
+    //    var url = encodeURI("/ss/" + key + "/");
         window.open(url, '_blank');
-    //}
+    }
 
 }
 function s_method() {

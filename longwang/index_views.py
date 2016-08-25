df4fe078b416299cef13622d6d8e9aee75aeafae
laurencechan <laurencechan.qq.com>
2016-08-25 17:26:28 +0800

    黑龙江网页面首页微调 龙江头条  4--->3 龙江看点   8--->4 new  龙江生活    5

--- longwang/index_views.py
+++ longwang/index_views.py
@@ -27,7 +27,7 @@ def index():
     # 轮换图
     lht = get_head_image(ObjectId("57688f50dcc88e552361ba27"), 5)
     # 龙江头条
-    yw = search_indexnews_db("576b36a9a6d2e970226062c3", 4)
+    yw = search_indexnews_db("576b36a9a6d2e970226062c3", 3)
     # 侃八卦
     gbg = search_indexnews_db("579190303c7ee91e3478823e", 10)
     # 专题
@@ -35,10 +35,10 @@ def index():
     zt = search_indexnews_db("579584633c7e431eaf791a06", 3)
     # 热门图集
     rmtj = search_indexnews_db("57bba817f5e86117cb228908", 5)
-    # 今日要闻
-    # gcdt = search_indexnews_db("576b3715a6d2e970226062c8", 4)
+    # 龙江生活
+    ljsh = search_indexnews_db("57beb8507fdf3f9496838594", 4)
     # 龙江看点
-    ljkd = search_indexnews_db("579190303c7ee91e3478823d", 8)
+    ljkd = search_indexnews_db("579190303c7ee91e3478823d", 5)
     # 新闻排行
     hours = search_indexnews_db("576b37b8a6d2e970226062d1", 8)
     zb = search_indexnews_db("576b37cda6d2e970226062d4", 8)
@@ -70,6 +70,7 @@ def index():
     for news_detail in news_list:
         _news_list.append(get_mongodb_dict(news_detail))
     return render_template('index.html', zt_images=zt_images, zt=zt, gbg=gbg, yw=yw, ljkd=ljkd, lht=lht,
+                           ljsh=ljsh,
                            rmtj=rmtj,
                            menu=get_menu(),
                            news_list=_news_list,

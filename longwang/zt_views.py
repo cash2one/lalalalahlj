bfe99f7c4476e1e94f2b448337319f5012cde265
eiffel <wanglina_@126.com>
2016-08-25 16:02:38 +0800

    site-->modify indexnews

--- longwang/zt_views.py
+++ longwang/zt_views.py
@@ -19,44 +19,44 @@ def zt_add(id):
         f = request.files['topImage3']
         _title, _ext = os.path.splitext(f.name)
         if f != "" and f != None:
-           try:
-            fext = str(f).lower().split(".")[1]
-            if fext == 'jpg' or fext == "png" or fext == "jpeg" or fext == "bmp":
+            try:
+                fext = str(f).lower().split(".")[1]
+                if fext == 'jpg' or fext == "png" or fext == "jpeg" or fext == "bmp":
                     sltName = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
                     name = sltName
                     uploadurl = upload_path(id + "/img/" + name + _ext)
                     f.save(uploadurl)
-            else:
-                if fext == "css":
-                    uploadurl = upload_path(id + "/css/" + _title + _ext)
-                    destination = open(uploadurl, 'wb+')
-                    for chunk in f.chunks():
-                        destination.write(chunk)
-                    name = _title
-                if fext == "js":
-                    uploadurl = upload_path(id + "/js/" + _title + _ext)
-                    destination = open(uploadurl, 'wb+')
-                    for chunk in f.chunks():
-                        destination.write(chunk)
-                    name = _title
-                if fext == "html":
-                    uploadurl = upload_path("/html/" + _title + _ext)
-                    destination = open(uploadurl, 'wb+')
-                    for chunk in f.chunks():
-                        destination.write(chunk)
-                    name = _title
-            insertinfo = {
-                "name": _title + _ext,
-                "url": uploadurl,
-                "name1": name + _ext,
-                "newsid": id,
-                "type": _ext,
-                "index": 0
-            }
-            pro.insert(insertinfo)
-            nid = str(pro.find_one({"newsid": id, "url": uploadurl})["_id"])
-           except:
-               return json.dumps({"status": 500})
+                else:
+                    if fext == "css":
+                        uploadurl = upload_path(id + "/css/" + _title + _ext)
+                        destination = open(uploadurl, 'wb+')
+                        for chunk in f.chunks():
+                            destination.write(chunk)
+                        name = _title
+                    if fext == "js":
+                        uploadurl = upload_path(id + "/js/" + _title + _ext)
+                        destination = open(uploadurl, 'wb+')
+                        for chunk in f.chunks():
+                            destination.write(chunk)
+                        name = _title
+                    if fext == "html":
+                        uploadurl = upload_path("/html/" + _title + _ext)
+                        destination = open(uploadurl, 'wb+')
+                        for chunk in f.chunks():
+                            destination.write(chunk)
+                        name = _title
+                insertinfo = {
+                    "name": _title + _ext,
+                    "url": uploadurl,
+                    "name1": name + _ext,
+                    "newsid": id,
+                    "type": _ext,
+                    "index": 0
+                }
+                pro.insert(insertinfo)
+                nid = str(pro.find_one({"newsid": id, "url": uploadurl})["_id"])
+            except Exception, e:
+                return json.dumps({"status": e.message})
         return json.dumps({'url': uploadurl, "status": 0, "name": name + _ext, "type": _ext, "id": nid})
     else:
         return json.dumps({"status": 400})

43e0ebce50481bd15a96aba5d338f9db47c05e9c
eiffel <wanglina_@126.com>
2016-08-27 15:11:18 +0800

    site-->modify indexnews

--- longwang/index_views.py
+++ longwang/index_views.py
@@ -168,7 +168,7 @@ def detail(id, page=1):
         if zt == None:
             return render_template("404.html")
         else:
-            return render_template(zt["url"])
+            return render_template(str(zt["url"]).replace("zuanti", "zt"))
     # 频道
     channel = db.Channel.find_one({"_id": ObjectId(detail["Channel"][0])})
     # 趣事秒闻

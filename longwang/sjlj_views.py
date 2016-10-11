4d7b457c567c08af41e4873783d9befd40e1502a
laurencechan <laurencechan.qq.com>
2016-10-11 09:49:22 +0800

    新增数据龙江页面---增加注释

--- longwang/sjlj_views.py
+++ longwang/sjlj_views.py
@@ -12,12 +12,18 @@ sjlj_page = Blueprint('sjlj_page', __name__, template_folder="templates")
 
 
 @sjlj_page.route("/sjlj/")
-def index():
+def index():  # 数据龙江首页
+    # 头图 5条
     tt = search_indexnews_db("57faed0ab9201c3c53b0ea89", 5)
+    # 精品推荐 4条
     jptj = search_indexnews_db("57faed0ab9201c3c53b0ea8a", 4)
+    # 最新发布 8条
     zxfb = search_indexnews_db("57faed0ab9201c3c53b0ea8b", 8)
+    # 增加点击次数和日期
     zxfb_new = list_new(zxfb)
+    # 往期回顾 8条
     wqhg = search_indexnews_db("57faed0ab9201c3c53b0ea8c", 8)
+    # 增加点击次数和日期
     wqhg_new = list_new(wqhg)
     return render_template('sjlj/index.html', name="数据龙江",
                            tt=tt,
@@ -31,12 +37,13 @@ def index():
 
 @sjlj_page.route('/sjlj/<id>.html/')
 @sjlj_page.route('/sjlj/<id>_<page>.html/')
-def sjlj_list(id, page=1):
-    pre_page = 10
-    channel = db.Channel.find_one({"numid": int(id)})
-    _id = channel["_id"]
+def sjlj_list(id, page=1):   # 数据龙江列表页
+    pre_page = 10  # 当前页最多新闻条数
+    channel = db.Channel.find_one({"numid": int(id)})  #频道内容
+    # _id = channel["_id"]
     list = search_news_db([channel["_id"]], 10)
     condition = {"Channel": {"$in": [channel["_id"]]}, "Status": 4}
+    # 翻页内容
     count = db.News.find(condition).sort('Pulished', pymongo.DESCENDING).count()
     news_list = db.News.find(condition).sort('Published', pymongo.DESCENDING).skip(pre_page * (int(page) - 1)).limit(
         pre_page)
@@ -53,6 +60,7 @@ def sjlj_list(id, page=1):
 
 
 def list_new(raw_list=[]):
+    # 在indexnews内容里增加发布时间和点击次数两项内容
     new_list = []
     for i in raw_list:
         raw_new = db.News.find_one({"numid": i["_id"]})
